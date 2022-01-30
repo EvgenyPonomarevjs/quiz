6004f5aae4b53c21b2887f53333c20473aae9ba1
unknown <ponomareve45@gmail.com>
2022-01-30 17:04:38 +0300

    added JSON file

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 const questions = [
-	{
-		question: "Какой язык работает в браузере?",
-		answers: ["Java", "C", "Python", "JavaScript"],
-		correct: 4,
-	},
-	{
-		question: "Что означает CSS?",
-		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-		correct: 2,
-	},
-	{
-		question: "Что означает HTML?",
-		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
-		],
-		correct: 1,
-	},
-	{
-		question: "В каком году был создан JavaScript?",
-		answers: ["1996", "1995", "1994", "все ответы неверные"],
-		correct: 2,
-	},
-	{
-		question: "В чем отличие между локальной и глобальной переменной?",
-		answers: [
-		"Локальные можно переопределять, глобальные нельзя", 
-		"Глобальные видны повсюду, локальные только в функциях", 
-		"Глобальные можно переопределять, локальные нельзя", 
-		"Локальные видны повсюду, глобальные только в функциях"],
-		correct: 2,
-	},
+// 	{
+// 		question: "Какой язык работает в браузере?",
+// 		answers: ["Java", "C", "Python", "JavaScript"],
+// 		correct: 4,
+// 	},
+// 	{
+// 		question: "Что означает CSS?",
+// 		answers: [
+// 			"Central Style Sheets",
+// 			"Cascading Style Sheets",
+// 			"Cascading Simple Sheets",
+// 			"Cars SUVs Sailboats",
+// 		],
+// 		correct: 2,
+// 	},
+// 	{
+// 		question: "Что означает HTML?",
+// 		answers: [
+// 			"Hypertext Markup Language",
+// 			"Hypertext Markdown Language",
+// 			"Hyperloop Machine Language",
+// 			"Helicopters Terminals Motorboats Lamborginis",
+// 		],
+// 		correct: 1,
+	// },
+	// {
+	// 	question: "В каком году был создан JavaScript?",
+	// 	answers: ["1996", "1995", "1994", "все ответы неверные"],
+	// 	correct: 2,
+	// },
+	// {
+	// 	question: "В чем отличие между локальной и глобальной переменной?",
+	// 	answers: [
+	// 	"Локальные можно переопределять, глобальные нельзя", 
+	// 	"Глобальные видны повсюду, локальные только в функциях", 
+	// 	"Глобальные можно переопределять, локальные нельзя", 
+	// 	"Локальные видны повсюду, глобальные только в функциях"],
+	// 	correct: 2,
+	// },
 	{
 		question: "Какое количество сообщений будет выведено в консоль?---------------------'for(var i = 10; i < 35; i += 5){console.log(i);}'",
 		answers: ["5", "10", "35", "8"],
@@ -76,6 +76,9 @@ const questions = [
 const headerConteiner = document.querySelector('#header');
 const listConteiner = document.querySelector('#list');
 const submitBtn = document.querySelector('#submit');
+const submitStart = document.querySelector('#submit-start');
+const modelClose = document.querySelector('.modal-result-wrap');
+const textJson1 = document.querySelector('.content1');
 
 let score = 0; //кол-во правльных ответов
 let questionIndex = 0; //тeкущий вопрос
@@ -88,6 +91,44 @@ function clearPage(){
 headerConteiner.innerHTML = '';
 listConteiner.innerHTML = '';	
 }
+
+submitStart.addEventListener('click', function () {
+	console.log('click');
+	modelClose.className = 'model-close';
+});
+
+
+
+// function newGame() {
+// 	clearPage();
+// 	const headerNewGame = `<h2 class="title">Тест на знание основ Javascript</h2>`;
+// 	headerConteiner.innerHTML = headerNewGame;
+// 	submitBtn.blur();
+// 	submitBtn.innerText = 'Начать тест';
+// };
+
+// function gameStart () {
+// 	submitBtn.onclick = () => {
+		
+// 	}
+// newGame();	
+// }
+// gameStart();
+
+
+//Подключение JSON
+const getData = () => fetch('https://jopandaverst.github.io/json')
+.then((res) => res.json())
+.then((res) => console.log(res))
+.then((data) => jsonData(data))
+.catch(error => console.error(error));
+
+
+
+function jsonData({question, answers, correct}) {
+	console.log(question);
+}
+getData()
 
 function showQuestion(){
 	//вопрос
@@ -169,3 +210,108 @@ function showResults() {
 	submitBtn.innerText = 'Начать заново';
 	submitBtn.onclick = () => history.go();
 }
+
+
+
+
+
+
+
+//Time
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+	info: {
+    color: "green"
+	},
+	warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+	},
+	alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+	}
+};
+
+const TIME_LIMIT = 120;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+	<svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+		<circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+		<path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+			M 50, 50
+			m -45, 0
+			a 45,45 0 1,0 90,0
+			a 45,45 0 1,0 -90,0
+        "
+	></path>
+    </g>
+	</svg>
+	<span id="base-timer-label" class="base-timer__label">${formatTime(timeLeft)}</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {clearInterval(timerInterval);}
+
+function startTimer() {
+	timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+		onTimesUp();
+    }
+}, 1000);
+}
+
+function formatTime(time) {
+	const minutes = Math.floor(time / 60);
+	let seconds = time % 60;
+
+	if (seconds < 10) {
+    seconds = `0${seconds}`;
+	}
+
+	return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+	const { alert, warning, info } = COLOR_CODES;
+	if (timeLeft <= alert.threshold) {
+    document
+	.getElementById("base-timer-path-remaining").classList.remove(warning.color);
+    document.getElementById("base-timer-path-remaining").classList.add(alert.color);
+	} else if (timeLeft <= warning.threshold) {
+    document.getElementById("base-timer-path-remaining").classList.remove(info.color);
+    document.getElementById("base-timer-path-remaining").classList.add(warning.color);
+	}
+}
+
+function calculateTimeFraction() {
+	const rawTimeFraction = timeLeft / TIME_LIMIT;
+	return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+	const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY).toFixed(0)} 283`;
+	document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
+};
